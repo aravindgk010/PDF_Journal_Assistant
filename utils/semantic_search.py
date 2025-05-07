6a34ded7792432983aef58263e18227924b95d8c
Aravind Gopal <gopa0025@algonquinlive.com>
2025-05-06 23:35:52 -0400

    added code to use the local model

--- utils/semantic_search.py
+++ utils/semantic_search.py
@@ -1,4 +1,4 @@
-from sentence_transformers import SentenceTransformer  #type: ignore
+from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 import os
